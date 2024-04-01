f94dd8eb9781157340c80dddb4a30695edbab2f6
CINTIAREISIFCE <cintia.reis@ifce.edu.br>
2024-04-01 17:48:07 -0300

    adiciona index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const soma = (n1, n2) => n1+n2
+const subtracao = (n1, n2) => n1-n2
+const multiplicacao = (n1, n2) => n1*n2
+const divisao = (n1, n2) => n1/n2
+
+export{
+    soma,
+    subtracao,
+    multiplicacao,
+    divisao,
+}
